053b2855a48d084ba468e82df39e087cb090660a
zhukai1998 <17079426651@163.com>
2021-06-28 23:56:38 +0800

    first commit

---
+++ src/main/java/com/zhukai/gospringboot/GoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.zhukai.gospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhukai/gospringboot/GoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhukai.gospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
